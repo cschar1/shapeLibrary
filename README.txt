a244b56648972d54263cfa26536e5fa9789c0ccc
unknown <cody.scharfe@ntc.norbord.com>
2010-06-07 12:47:30 -0400

    initial shapeLIbrary commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+ShapelIbrary for use with other projects,
+hosted on github to use as a submodule with other repositories.
---
+++ shapeLibrary/UserControl1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace shapeLibrary
+{
+    public partial class UserControl1 : UserControl
+    {
+        private int rWidth;
+        private int rHeight;
+        bool increasing;
+        public UserControl1()
+        {
+            InitializeComponent();
+            rWidth = 30;
+            rHeight = 30;
+            increasing = true;
+            timer1.Start();
+            timer1.Interval = 100;
+        }
+
+        private void UserControl1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(Pens.Coral, new Rectangle(30, 30, rWidth, rHeight));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (rWidth >= 50 || rWidth <= 25)
+                increasing = !increasing;
+
+            if (increasing)
+            {
+                rWidth += 5;
+                rHeight += 5;
+            }
+            else
+            {
+                rWidth -= 5;
+                rHeight -= 5;
+            }
+
+            this.Invalidate();
+            
+        }
+    }
+}
